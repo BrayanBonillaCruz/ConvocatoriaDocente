7e3c736edce434cf49c7be8a885bfc7edf2f29ba
Brayan <jj_jkl@outlook.es>
2023-06-06 11:35:22 -0600

    🥝 Materialize

--- client/index.js
+++ client/index.js
@@ -1,4 +1,12 @@
 /* eslint-disable no-console */
 import './styles/style.css';
+/* eslint-disable */
+// Importandoe Estilos de Materialize CSS
+import 'materialize-css/dist/css/materialize.css';
+// Importando scripts de Materialize
+import 'materialize-css/dist/js/materialize';
+/* eslint-enable */
+// Inicializando Scripts de Materialize para interactividad
+M.AutoInit();
 
-console.log('🎉 Estilos cargados correctamente 🎉');
+console.log('Webpack Working Middleware!!! 📦');
