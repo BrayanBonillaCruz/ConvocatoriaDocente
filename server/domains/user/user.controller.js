18a794c51d34c02543189c1b8246a5a9b7a39e02
Brayan <jj_jkl@outlook.es>
2023-06-09 21:02:27 +0000

    🍕 Reconstruction

--- server/domains/user/user.controller.js
+++ server/domains/user/user.controller.js
@@ -1,4 +1,3 @@
-import log from '../../config/winston';
 // Actions methods
 // GET "/user/login"
 const login = (req, res) => {
@@ -15,40 +14,10 @@ const register = (req, res) => {
   res.render('user/register');
 };
 
-// POST "/user/register"
-const addPost = (req, res) => {
-  // Rescatando la info del formulario
-  const { errorData: validationError } = req;
-  // En caso de haber error
-  // se le informa al cliente
-  if (validationError) {
-    log.info('Se entrega al cliente error de validación de add User');
-    // Se desestructuran los datos de validación
-    const { value: user } = validationError;
-    // Se extraen los campos que fallaron en la validación
-    const errorModel = validationError.inner.reduce((prev, curr) => {
-      // Creando una variable temporal para
-      // evitar el error "no-param-reassing"
-      const workingPrev = prev;
-      workingPrev[`${curr.path}`] = curr.message;
-      return workingPrev;
-    }, {});
-    return res.status(422).render('user/register', { user, errorModel });
-  }
-  // En caso de que pase la validación
-  // Se desestructura la información
-  // de la peticion
-  const { validData: project } = req;
-  // Se contesta la información
-  // del proyecto al cliente
-  log.info('Se entrega al cliente información del usuario cargado');
-  return res.status(200).json(project);
-};
 // Controlador Home
 export default {
   // Action Methods
   login,
   logout,
   register,
-  addPost,
 };
--- server/domains/user/user.router.js
+++ server/domains/user/user.router.js
@@ -4,11 +4,6 @@ import { Router } from 'express';
 // Importando el controlador
 import userController from './user.controller';
 
-// Importando factory de validación
-import ValidateFactory from '../../services/validateFactory';
-// Importando el validador de proyectos
-import userValidator from './user.validator';
-
 // Creando una isntancia del enrutador
 const router = new Router();
 
@@ -19,15 +14,6 @@ router.get('/login', userController.login);
 router.get('/logout', userController.logout);
 // GET '/user/register
 router.get('/register', userController.register);
-// POST "/user/register"
-router.post(
-  '/register',
-  ValidateFactory({
-    schema: userValidator.registerSchema,
-    getObject: userValidator.getRegister,
-  }),
-  userController.addPost
-);
 
 // Exporto este tramo de ruta
 export default router;
